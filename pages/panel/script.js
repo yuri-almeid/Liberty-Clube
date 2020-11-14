caf6f223f1ecb5c9cf07aa2ccfbc72a1c8ff2bd3
Yuri Almeida <yurilima95@gmail.com>
2020-11-13 23:30:43 -0300

    friday 13 update

--- pages/panel/script.js
+++ pages/panel/script.js
@@ -1,42 +1,42 @@
 
-// function renderChart(data, labels) {
-//   var ctx = document.getElementById("myChart").getContext('2d');
-//   var myChart = new Chart(ctx, {
-//       type: 'line',
-//       data: {
-//           labels: labels,
-//           datasets: [{
-//               label: 'This week',
-//               data: data,
-//           }]
-//       },
-//   });
-// }
+function renderChart(data, labels) {
+  var ctx = document.getElementById("myChart").getContext('2d');
+  var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          labels: labels,
+          datasets: [{
+              label: 'This week',
+              data: data,
+          }]
+      },
+  });
+}
 
-// $("#loadData").click(
-//   function () {
-//       data = [20000, 14000, 12000, 15000, 18000, 19000, 22000];
-//       labels =  ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-//       renderChart(data, labels);
-//   }
-// );
+$("#loadData").click(
+  function () {
+      data = [20000, 14000, 12000, 15000, 18000, 19000, 22000];
+      labels =  ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+      renderChart(data, labels);
+  }
+);
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
+// var ctx = document.getElementById('myChart').getContext('2d');
+// var chart = new Chart(ctx, {
+//     // The type of chart we want to create
+//     type: 'line',
 
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
+//     // The data for our dataset
+//     data: {
+//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//         datasets: [{
+//             label: 'My First dataset',
+//             backgroundColor: 'rgb(255, 99, 132)',
+//             borderColor: 'rgb(255, 99, 132)',
+//             data: [0, 10, 5, 2, 20, 30, 45]
+//         }]
+//     },
 
-    // Configuration options go here
-    options: {}
-});
+//     // Configuration options go here
+//     options: {}
+// });
